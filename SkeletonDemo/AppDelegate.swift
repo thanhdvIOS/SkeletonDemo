3869296bbbf310c3c83893a2feba215e6710dfce
Đỗ Văn Thanh <thanhdv@ominext.com>
2017-10-13 11:02:01 +0700

    commit branch2

--- SkeletonDemo/AppDelegate.swift
+++ SkeletonDemo/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
     window?.makeKeyAndVisible()
     window?.rootViewController = UINavigationController(rootViewController: ViewController())
-    
+    // dang o nhanh 2
     return true
   }
 
